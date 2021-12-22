4c43d5721a90bf77319d01acc87d0b97df6b02f4
srinidhi <srinidhigs1997@gmail.com>
2021-12-22 14:24:45 +0530

    new and create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,27 @@ class TodosController < ApplicationController
    		@todo = Todo.find(params[:id])
   	end
 
+  	def new
+    	@todo = Todo.new
+    end
+
+    def create
+    	@todo = Todo.new(todo_params)
+
+        if @todo.save
+          redirect_to todos_path, notice: "Task was successfully created"
+        else
+          render :new 
+ 	      end
+	end
+
+	private
+
+  	def todo_params
+
+      	params.require(:todo).permit(:task, :description, :date)
+
+  	end
+
+
 end
