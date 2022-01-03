58fe95b59fa04e36c6fca544ea8302dde10ee6b2
srinidhi <srinidhigs1997@gmail.com>
2022-01-03 11:08:07 +0530

    final commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,65 +1,62 @@
 class TodosController < ApplicationController
 	 before_action :set_todo, only: %i[ show edit update destroy ]
 	 before_action :authenticate_user!,expect: %i[index show]
-	 before_action :correct_user,only: %i[edit update destroy]
+	 before_action :correct_user,only: %i[ show edit update destroy]
+
 	def index
 		@todos = Todo.all
 	end
 
 	def show
    		
-  	end
+  end
 
-  	def new
-    	@todo = current_user.todos.build
-    end
+	def new
+  	@todo = current_user.todos.build
+  end
 
-    def create
-    	@todo = current_user.todos.build(todo_params)
-
-        if @todo.save
-          redirect_to todos_path, notice: "Task was successfully created"
-
-        else
-          render :new,notice: "just check something is wrong" 
-
- 	    end
+ 	def create
+    @todo = current_user.todos.build(todo_params)
+    	if @todo.save
+        redirect_to todos_path, notice: "Task was successfully created"
+      else
+        render :new,notice: "just check something is wrong" 
+	    end
 	end
 
 	def edit
 	
-    end
+  end
 
-  	def update
-    	
+  def update
     	if @todo.update(todo_params)
       	redirect_to todos_path, notice: "Task was successfully updated"
 
    		else
-     	render :edit
+     		render :edit
     	end
 
-    end
+  end
 
-		def destroy
+	def destroy
     	@todo.destroy
     	redirect_to todos_path,notice: "Task was successfully deleted"
-  	end
+  end
 
-  	def correct_user
+  def correct_user
   		@todo = current_user.todos.find_by(id: params[:id])
   		redirect_to todos_path, notice: "Heyy!You are not authorized" if @todo.nil?
-  	end
+  end
 
 	private
 
 	def set_todo
-		@todo = Todo.find(params[:id])
+			@todo = Todo.find(params[:id])
 	end
 
-  	def todo_params
+  def todo_params
 
-      	params.require(:todo).permit(:task, :description, :date, :user_id)
+      params.require(:todo).permit(:task, :description, :date)
 
-  	end
+  end
 end
